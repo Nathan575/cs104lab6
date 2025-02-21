22c092f6404053b15cce706c9c72bd51b757f9fa
Nathan575 <59374917+Nathan575@users.noreply.github.com>
2025-02-21 13:36:22 -0800

    Added randfuncs.cpp and randfuncs.h files

---
+++ randfuncs.h
@@ -0,0 +1,9 @@
+#ifndef RANDFUNCS_H
+#define RANDFUNCS_H
+
+void seed_random();  // Function to seed the random number generator
+std::string flip_coin();
+int roll_six_sided_die();
+int roll_twenty_sided_die();
+
+#endif
